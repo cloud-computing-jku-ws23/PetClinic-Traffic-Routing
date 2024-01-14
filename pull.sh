086485acaf1a06c3a605e40b012314584a75bda6
Kornek <alex-do2011@hotmail.de>
2024-01-14 19:07:40 +0100

    change: deployment

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose pull
-kind load docker-image springcommunity/spring-petclinic-discovery-server springcommunity/spring-petclinic-config-server springcommunity/spring-petclinic-api-gateway springcommunity/spring-petclinic-customers-service springcommunity/spring-petclinic-visits-service springcommunity/spring-petclinic-vets-service
+kind load docker-image springcommunity/spring-petclinic-cloud-api-gateway springcommunity/spring-petclinic-cloud-customers-service springcommunity/spring-petclinic-cloud-vets-service springcommunity/spring-petclinic-cloud-visits-service
