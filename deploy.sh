564681fdefb3e855a9fde7ae3882f47b246fbde9
Kornek <alex-do2011@hotmail.de>
2024-01-14 17:44:15 +0100

    add: microservices are working

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-kubectl apply -f "./deployments/api-gateway-deployment.yaml"
 kubectl apply -f "./deployments/config-server-deployment.yaml"
-kubectl apply -f "./deployments/customers-service-deployment.yaml"
 kubectl apply -f "./deployments/discovery-server-deployment.yaml"
+kubectl apply -f "./deployments/api-gateway-deployment.yaml"
+kubectl apply -f "./deployments/customers-service-deployment.yaml"
 kubectl apply -f "./deployments/vets-service-deployment.yaml"
 kubectl apply -f "./deployments/visits-service-deployment.yaml"
+
+kubectl apply -f "./services/config-server-service.yaml"
+kubectl apply -f "./services/discovery-server-service.yaml"
+kubectl apply -f "./services/api-gateway-service.yaml"
+kubectl apply -f "./services/customers-service-service.yaml"
+kubectl apply -f "./services/vets-service-service.yaml"
+kubectl apply -f "./services/visits-service-service.yaml"
